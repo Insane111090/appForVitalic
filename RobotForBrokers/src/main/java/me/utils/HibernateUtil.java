e8fc31ae81cbd2d3659203186e2ba821b1c81ec9
Андрей <volirvag132005@bk.ru>
2014-04-11 00:13:22 +0400

    Tables created

--- RobotForBrokers/src/main/java/me/utils/HibernateUtil.java
+++ RobotForBrokers/src/main/java/me/utils/HibernateUtil.java
@@ -8,12 +8,6 @@ import java.util.Currency;
 public class HibernateUtil {
     private static SessionFactory sessionFactory = null;
 
-    Session s = new HibernateUtil();
-    s.getInstance().updateData(Set<Object>);
-
-    Currency obj = new Currency();
-    List<Currecncy> list = obj.getIntance().startSeesion()getAll().getElemtsByName();
-    calc(list);
 
 
 }
--- Scripts/DB/2.Create tables.sql
+++ Scripts/DB/2.Create tables.sql
@@ -1,14 +1,51 @@
-create table currency(
-currency_id number(20) not null,
-currency_name varchar2(200) not null,
+drop table currency_ask;
+/
+drop table currency_bid;
+/
+drop table currency;
+/
+drop table date_info;
+/
+create table currency
+(currency_id number(20) not null,
+currency_name varchar2(200) ,
+constraint pk_currency_id primary key (currency_id));
+/
+create table date_info
+(date_id number(20) not null,
 start_time timestamp not null,
 end_time timestamp not null,
-constraint currency_pk primary key(currency_id));
+constraint pk_date_id primary key (date_id));
+/
+create table currency_ask
+(currency_id number(20) not null,
+date_id number(20) not null,
+open_price varchar2(20) not null,
+close_price varchar2(20) not null,
+constraint fk_date_ask foreign key (date_id)
+references date_info(date_id) on delete cascade,
+constraint fk_currency_ask foreign key (currency_id)
+references currency(currency_id)on delete cascade);
+/
+create table currency_bid
+(currency_id number(20) not null,
+date_id number(20) not null,
+open_price varchar2(20) not null,
+close_price varchar2(20) not null,
+constraint fk_date_bid foreign key (date_id)
+references date_info(date_id) on delete cascade,
+constraint fk_currency_bid foreign key (currency_id)
+references currency(currency_id)on delete cascade);
+/
+create index date_index_bid
+on currency_bid(date_id);
+/
+create index date_index_ask
+on currency_ask(date_id);
+/
+create index currency_index_bid
+on currency_bid(currency_id);
+/
+create index currency_index_ask
+on currency_ask(currency_id);
 
-create table currency_info(
-currency_id number(20) not null,
-open_price varchar2(20),
-close_price varchar2(20),
-constraint fk_currency_info
-foreign key(currency_id)
-references currency (currency_id));
